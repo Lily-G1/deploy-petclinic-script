2cffc884902c4fb31b9ed1d00a4ef821bc8853aa
Lilian Galadima <anyagaladima@gmail.com>
2023-05-18 18:25:05 +0100

    add shell script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install git -y
+sudo yum install java-17-amazon-corretto-devel -y
+sleep 60
+git clone https://github.com/Lily-G1/spring-petclinic.git
+cd spring-petclinic/
+./mvnw package
+java -jar target/*.jar
